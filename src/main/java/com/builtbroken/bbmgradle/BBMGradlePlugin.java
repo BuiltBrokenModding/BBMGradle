dc0d0657e8c08b5a475d2701cbd5ba3501c23007
Ole Henrik Stabell <ole@henrikstabell.com>
2018-03-20 23:03:30 +0100

    Add initial src code

---
+++ src/main/java/com/builtbroken/bbmgradle/BBMGradlePlugin.java
@@ -0,0 +1,16 @@
+package com.builtbroken.bbmgradle;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Hennamann(Ole Henrik Stabell) on 20/03/2018.
+ */
+public class BBMGradlePlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+
+    }
+}
